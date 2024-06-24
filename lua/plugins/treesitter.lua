9e6d42e21e56c930948ec230f1c603acf87e0ef6
nika.pruidze <nika.pruidze1@gmail.com>
2024-06-24 14:44:23 +0400

    first commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,77 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "cmake",
+        "diff",
+        "http",
+        "html",
+        "css",
+        "scss",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "graphql",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "astro",
+        "fish",
+        "gitignore",
+        "rust",
+        "go",
+        "java",
+        "php",
+        "sql",
+        "svelte",
+        "vue",
+        "prisma",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
