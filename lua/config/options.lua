80c872d2d27080fd408086c90c18b6b46545cd82
nika.pruidze <nika.pruidze1@gmail.com>
2025-01-04 04:35:22 +0400

    disable animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Undercurl
--- TODO: find out how to add undercurl for iterm I'm using
+vim.g.snacks_animate = false
